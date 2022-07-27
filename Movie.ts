24e6a913660cdaa62275c0bb6ed6557dfa49d530
dianacastano <diamarcast1803@gmail.com>
2022-07-27 17:10:44 +0200

    cambio final

--- Movie.ts
+++ Movie.ts
@@ -24,75 +24,15 @@ class Movie {
 
     }
 
-    public setactors(actor: Professional[]):void {
-        this.actors = actor;
-    }
-
-    public getactors(){
-        return this.actors;
-    }
-
-    public setwriter(writ: Professional[]):void {
-        this.writer= writ;
-    }
-
-    public getwriter(){
-        return this.writer;
-    }
-
-    public setlanguage(lengua: string):void {
-        this.language= lengua;
-    }
-
-    public getlanguage(){
-        return this.language;
-    }
-
-    public setplataform(plataforma: string):void {
-        this.plataform = plataforma;
-    }
-
-    public getplataform(){
-        return this.plataform;
-    }
-
-    public setisMCU(MCU: boolean):void {
-        this.isMCU = MCU;
-    }
-
-    public getisMCU(){
-        return this.isMCU;
-    }
-
-    public setmainCharacterName(CharacterName: string):void {
-        this.mainCharacterName = CharacterName;
-    }
-
-    public getmainCharacterName(){
-        return this.mainCharacterName;
-    }
-
-    public setproducer(productor: Professional[]):void {
-        this.producer = productor;
-    }
-
-    public getproducer(){
-        return this.producer;
-    }
-
-    public setdistributor(distribuidor: string):void {
-        this.distributor = distribuidor;
-    }
-
-    public getdistributor(){
-        return this.distributor;
-    }
-
-    public printAll (): void{
-        console.log( this.title, this.releaseYear, this.actors, this.nacionality, this.writer,
-             this.language, this.plataform, this.isMCU, this.mainCharacterName, this.producer, 
-             this.distributor, this.genre );
-       }
+    public printAll (){
+        
+        for (let prop in this) {
+            if (prop != "print") {
+              console.log(`${prop} - ${eval("this." + prop)}`);
+            }
+          }
+          console.log('');
+        }
    
 
 }
