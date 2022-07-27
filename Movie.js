4fabe8dea7db29a73cb2baa2d67afc0d2cca8320
dianacastano <diamarcast1803@gmail.com>
2022-07-27 17:16:08 +0200

    cambio final

--- Movie.js
+++ Movie.js
@@ -8,56 +8,13 @@ var Movie = /** @class */ (function () {
         this.nacionality = nacionality;
         this.genre = genre;
     }
-    Movie.prototype.setactors = function (actor) {
-        this.actors = actor;
-    };
-    Movie.prototype.getactors = function () {
-        return this.actors;
-    };
-    Movie.prototype.setwriter = function (writ) {
-        this.writer = writ;
-    };
-    Movie.prototype.getwriter = function () {
-        return this.writer;
-    };
-    Movie.prototype.setlanguage = function (lengua) {
-        this.language = lengua;
-    };
-    Movie.prototype.getlanguage = function () {
-        return this.language;
-    };
-    Movie.prototype.setplataform = function (plataforma) {
-        this.plataform = plataforma;
-    };
-    Movie.prototype.getplataform = function () {
-        return this.plataform;
-    };
-    Movie.prototype.setisMCU = function (MCU) {
-        this.isMCU = MCU;
-    };
-    Movie.prototype.getisMCU = function () {
-        return this.isMCU;
-    };
-    Movie.prototype.setmainCharacterName = function (CharacterName) {
-        this.mainCharacterName = CharacterName;
-    };
-    Movie.prototype.getmainCharacterName = function () {
-        return this.mainCharacterName;
-    };
-    Movie.prototype.setproducer = function (productor) {
-        this.producer = productor;
-    };
-    Movie.prototype.getproducer = function () {
-        return this.producer;
-    };
-    Movie.prototype.setdistributor = function (distribuidor) {
-        this.distributor = distribuidor;
-    };
-    Movie.prototype.getdistributor = function () {
-        return this.distributor;
-    };
     Movie.prototype.printAll = function () {
-        console.log(this.title, this.releaseYear, this.actors, this.nacionality, this.writer, this.language, this.plataform, this.isMCU, this.mainCharacterName, this.producer, this.distributor, this.genre);
+        for (var prop in this) {
+            if (prop != "print") {
+                console.log("".concat(prop, " - ").concat(eval("this." + prop)));
+            }
+        }
+        console.log('');
     };
     return Movie;
 }());
